0868c6a481c8710257d384ed1d061de2b77de6d2
Marcelo Ramirez <ramirezyapura@gmail.com>
2023-11-23 09:52:58 -0400

    printea con variable y sin variable

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function analizarCodigo() {
     const codigo = document.getElementById('codigo').value;
     try {
         const resultado = miParser.parse(codigo);
-        document.getElementById('resultado').innerText = "Resultado: " + resultado.value;
+        if(resultado.value == undefined){
+            document.getElementById('resultado').innerText = "Resultado: " + resultado;         
+        }else{
+            document.getElementById('resultado').innerText = "Resultado: " + resultado.value;
+        }
     } catch (error) {
         document.getElementById('resultado').innerText = "Error de análisis: " + error.message;
     }
