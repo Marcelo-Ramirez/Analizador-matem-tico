7dc0bc913c1bd9bad78bb32bd32b164c0bb16c17
Marcelo Ramirez <ramirezyapura@gmail.com>
2023-11-29 19:00:58 -0400

    solucion a la ubicacion del archivo index.html

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 80; // Cambiado a puerto 80
 
-// Middleware para servir archivos estáticos desde la carpeta "public"
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware para servir archivos estáticos desde la misma ubicación que app.js (__dirname)
+app.use(express.static(__dirname));
 
+// Ruta para servir el archivo index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Ajustado la ruta del archivo HTML
+  res.sendFile(path.join(__dirname, 'index.html')); // Ajustado la ruta del archivo HTML
 });
 
 app.listen(port, () => {
