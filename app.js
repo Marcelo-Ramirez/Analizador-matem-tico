d2e47583545cfeb30403c02a88dca635f86108aa
Marcelo Ramirez <ramirezyapura@gmail.com>
2023-11-29 18:50:57 -0400

    cambiando el puerto al 80 para no especificar el puerto en el dominio

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80; // Cambiado a puerto 80
 
-// Middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname)));
+// Middleware para servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Ajustado la ruta del archivo HTML
 });
 
 app.listen(port, () => {
