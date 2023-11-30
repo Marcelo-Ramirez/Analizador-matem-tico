6cd1a4bd0ccdce4c200b509b9d5a810d16d7f874
Marcelo Ramirez <ramirezyapura@gmail.com>
2023-11-30 10:35:29 -0400

    solucion a al ver la ip de los que se conectan al servidor ya que app.use express.static tenia un conflicto en su orden

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 80; // Cambiado a puerto 80
+const port = process.env.PORT || 80;
 
-// Middleware para servir archivos estáticos desde la misma ubicación que app.js (__dirname)
-app.use(express.static(__dirname));
-
-// Ruta para servir el archivo index.html
+// Ruta para servir el archivo index.html y registrar la dirección IP
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html')); // Ajustado la ruta del archivo HTML
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`Dirección IP: ${ip}`);
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+// Middleware para servir archivos estáticos
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
   console.log(`El servidor está escuchando en el puerto ${port}`);
